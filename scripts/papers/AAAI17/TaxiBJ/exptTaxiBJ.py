022c76d7861e7ba6b7c75e240f12586eec1270a0
xu-wang11 <wangxu.93@icloud.com>
2017-02-21 16:42:49 +0800

    add taxt.js

--- scripts/papers/AAAI17/TaxiBJ/exptTaxiBJ.py
+++ scripts/papers/AAAI17/TaxiBJ/exptTaxiBJ.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-""" 
+"""
 Usage:
     THEANO_FLAGS="device=gpu0" python exptTaxiBJ.py [number_of_residual_units]
 """
 from __future__ import print_function
 import os
 import sys
-import cPickle as pickle
+import pickle as pickle
 import time
 import numpy as np
 import h5py
@@ -81,7 +81,7 @@ def read_cache(fname):
     f = h5py.File(fname, 'r')
     num = int(f['num'].value)
     X_train, Y_train, X_test, Y_test = [], [], [], []
-    for i in xrange(num):
+    for i in range(num):
         X_train.append(f['X_train_%i' % i].value)
         X_test.append(f['X_test_%i' % i].value)
     Y_train = f['Y_train'].value
